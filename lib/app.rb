2b6be46a9cccef4d84b5d11e90df702c66ee0d50
nekanayaka <nekanayaka@hollandcollege.com>
2016-05-04 15:25:18 +0000

    Added the ASCII arts

--- lib/app.rb
+++ lib/app.rb
@@ -5,9 +5,25 @@ products_hash = JSON.parse(file)
 
 # Print "Sales Report" in ascii art
 
+puts " _________      .__                  __________                             __   
+ /   _____/____  |  |   ____   ______ \______   \ ____ ______   ____________/  |_ 
+ \_____  \\__  \ |  | _/ __ \ /  ___/  |       _// __ \\____ \ /  _ \_  __ \   __\
+ /        \/ __ \|  |_\  ___/ \___ \   |    |   \  ___/|  |_> >  <_> )  | \/|  |  
+/_______  (____  /____/\___  >____  >  |____|_  /\___  >   __/ \____/|__|   |__|  
+        \/     \/          \/     \/          \/     \/|__|                       "
+
 # Print today's date
 
 # Print "Products" in ascii art
+
+puts "                     _            _       "
+puts "                    | |          | |      "
+puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
+puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
+puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
+puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
+puts "| |                                       "
+puts "|_|                                       "
 
 # For each product in the data set:
 	# Print the name of the toy
@@ -18,6 +34,14 @@ products_hash = JSON.parse(file)
 	# Calculate and print the average discount (% or $) based off the average sales price
 
 # Print "Brands" in ascii art
+
+puts " _                         _     "
+	puts "| |                       | |    "
+	puts "| |__  _ __ __ _ _ __   __| |___ "
+	puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
+	puts "| |_) | | | (_| | | | | (_| \\__ \\"
+	puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
+	puts
 
 # For each brand in the data set:
 	# Print the name of the brand
